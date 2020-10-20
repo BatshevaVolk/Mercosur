1f785970ec1869c4eb90b2e4ddb51146e47e0a8e
Nadav Fridberg <nadavf@fritz.co.il>
2020-10-20 14:47:09 +0300

    add paste and paste shift

--- src/app/components/form-details/form-details.component.ts
+++ src/app/components/form-details/form-details.component.ts
@@ -1,5 +1,6 @@
 
 import { Component, Input, ViewChild, AfterViewInit, HostListener } from '@angular/core';
+import { getMatFormFieldMissingControlError } from '@angular/material';
 import Handsontable from 'handsontable';
 const t: object = new Object();
 @Component({
@@ -12,7 +13,11 @@ export class FormDetailsComponent implements AfterViewInit {
   @ViewChild("handsontable", { static: false }) handsontable;
   @Input() data
   customContextMenu = {
-    items: {
+    items: this.getItems()
+  }
+  getItems() {
+    const comp = this;
+    return {
       'row_above': { name: 'Insert row above' },
       'row_below': { name: 'Insert row below' },
       'remove_row': { name: 'Remove row' },
@@ -21,49 +26,38 @@ export class FormDetailsComponent implements AfterViewInit {
       'paste': {
         name: 'Paste',
         callback: function (key, options) {
-          const pasteOptions = options;
-          const core = this;
-          navigator.clipboard.readText().then(
-            (clipText) => {
-              let data = [];
-              clipText.split("\n").forEach(row => {
-                if (row != "") {
-                  data.push(row.split("\t"))
-                }
-              });
-              for (var rowIndex = pasteOptions[0].start.row,dataRowIndex=0; rowIndex < data.length+pasteOptions[0].start.row; rowIndex += 1,dataRowIndex++) {
-                for (var columnIndex = pasteOptions[0].start.col,dataColumnIndex=0; columnIndex < data[0].length+pasteOptions[0].start.col; columnIndex += 1,dataColumnIndex++) {
-                  core.setDataAtCell(rowIndex, columnIndex, data[dataRowIndex][dataColumnIndex])
-                }
-              }
-            });
+          comp.paste(this, options, false);
         }
       },
       'paste_shift_cells_down': {
         name: 'Paste & Shift cells down',
         callback: function (key, options) {
-          const pasteOptions = options;
-          const core = this;
-          navigator.clipboard.readText().then(
-            (clipText) => {
-              let data = [];
-              clipText.split("\n").forEach(row => {
-                if (row != "") {
-                  data.push(row.split("\t"))
-                }
-              });
-              core.alter("insert_row",pasteOptions[0].start.row,data.length);
-              for (var rowIndex = pasteOptions[0].start.row,dataRowIndex=0; rowIndex < data.length+pasteOptions[0].start.row; rowIndex += 1,dataRowIndex++) {
-                for (var columnIndex = pasteOptions[0].start.col,dataColumnIndex=0; columnIndex < data[0].length+pasteOptions[0].start.col; columnIndex += 1,dataColumnIndex++) {
-                  core.setDataAtCell(rowIndex, columnIndex, data[dataRowIndex][dataColumnIndex])
-                }
-              }
-            });
+          comp.paste(this, options, true);
         }
       }
     }
   }
-
+  paste(coreObject, options, isShiftPaste) {
+    const pasteOptions = options;
+    const core = coreObject;
+    navigator.clipboard.readText().then(
+      (clipText) => {
+        let data = [];
+        clipText.split("\n").forEach(row => {
+          if (row != "") {
+            data.push(row.split("\t"))
+          }
+        });
+        if (isShiftPaste) {
+          core.alter("insert_row", pasteOptions[0].start.row, data.length);
+        }
+        for (var rowIndex = pasteOptions[0].start.row, dataRowIndex = 0; rowIndex < data.length + pasteOptions[0].start.row; rowIndex += 1, dataRowIndex++) {
+          for (var columnIndex = pasteOptions[0].start.col, dataColumnIndex = 0; columnIndex < data[0].length + pasteOptions[0].start.col; columnIndex += 1, dataColumnIndex++) {
+            core.setDataAtCell(rowIndex, columnIndex, data[dataRowIndex][dataColumnIndex])
+          }
+        }
+      });
+  }
   save() {
     let descriptionOfGoods = []
     let dataToSave = this.handsontable.hotInstance.getData();
